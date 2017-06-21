d8cf0e2e0863c1a3366d4b2258ff20691a8a474c
Gaurav Arora <gauravarora.daiict@gmail.com>
2017-06-21 19:57:06 +0530

    redirect only once ourselv not via devise

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,7 +3,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # You need to implement the method below in your model (e.g. app/models/user.rb)
       @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      sign_in @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Strava") if is_navigational_format?
       redirect_to user_path
     else
